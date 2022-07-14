2d27cf91f07e33925622f31e38cd9f69e800158d
msmolinski <msmolinski.2001@wp.pl>
2022-07-14 12:15:49 +0200

    4 commit

--- Zad_1.sh
+++ Zad_1.sh
@@ -3,12 +3,14 @@
 if [ $1 = "--date" ]
 then
 	date
+elif [ $1 = "--logs" ]
+then
+	x=1
+	while [ $x -le $2 ]
+	do
+		echo log$x > log$x.txt
+		echo Zad_1.sh >> log$x.txt
+		date >> log$x.txt
+		x=$((x+1))
+	done
 fi
-x=1
-while [ $x -le 100 ]
-do
-	echo log$x > log$x.txt
-	echo Zad_1.sh >> log$x.txt
-	date >> log$x.txt
-	x=$((x+1))
-done
